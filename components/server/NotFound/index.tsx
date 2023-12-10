025271052276473bad4a190f6b05524c5dd551e4
vinyl810 <19thsummer@kaist.ac.kr>
2023-12-10 19:16:07 +0900

    feat: add loading page, style: fix notfound and loading style

--- components/server/NotFound/index.tsx
+++ components/server/NotFound/index.tsx
@@ -4,7 +4,9 @@ import style from './style.module.css';
 export default function NotFound() {
   return (
     <div className={style['not-found']}>
-      404!
+      <h1 className={style['not-found-text']}>
+        404!
+      </h1>
     </div>
   );
 }
