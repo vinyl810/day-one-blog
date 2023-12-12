15d151df14e4a7fdda0bc1b27a3f96dce93af20c
Daewon Kim <52730665+vinyl810@users.noreply.github.com>
2023-12-12 22:07:52 +0900

    Feat/cover image (#11)
    * feat: add frontmatter to readContent and quick markup on cover image
    
    * fix: style automatic cover image height

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import readMarkdown from '@/lib/readMarkdown';
 import NotFound from '@/components/server/NotFound';
 import HighlightCode from '@/components/client/HighlightCode';
@@ -8,10 +9,22 @@ import style from './style.module.css';
 const { readContent } = readMarkdown();
 
 export default function Page({ params }: { params: { slug: string } }) {
-  const content = readContent(params.slug);
+  const { data, content } = readContent(params.slug) ?? { data: null, content: '' };
+  console.log(data?.title);
+
   return content
     ? (
       <div className={style['markdown-wrapper']}>
+        <h1>{data?.title}</h1>
+        <div
+          className={style['cover-image']}
+        >
+          <Image
+            src={data?.coverImage ?? ''}
+            fill
+            alt="coverImage"
+          />
+        </div>
         <Markdown>
           {content}
         </Markdown>
